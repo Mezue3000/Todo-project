133da819e0d6d273fdd74cf2ec7dd59792fb0bca
mezue30000 <mezue3000>
2023-12-21 22:15:54 +0100

    looped through array

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const Array1 = [];
+// create an empty array
+const myTodo = ['cook food', 'wash car'];
+renderList()
 
+// create a function for add button
 function addTodo () {
     const inputElement = document.querySelector('.js-todo')
-    const name = inputElement.value;
-    Array1.push(name);
-    console.log(Array1)
-}
+    const toDo = inputElement.value
+    myTodo.push(toDo)
+    inputElement.value = ''
+    renderList()
+
+}   
+//   loop through the array
+   function renderList () {
+    todoHtml = ''
+    for (i = 0; i < myTodo.length; i++) {
+        const todoList = myTodo[i]
+        const html = `<p>${todoList}</p>`
+        todoHtml += html  
+    }
+    
+   document.querySelector('.todo-app').innerHTML = todoHtml
+
+   }
